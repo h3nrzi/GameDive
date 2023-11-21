94aa9befc4620778c61757da181bcf24298bd0ad
Hossein <rezaeig22@gmail.com>
2023-11-21 18:18:13 +0330

    Bulid a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 const App = () => {
 	return (
-		<div>
-			<Button colorScheme="blue">Button</Button>
-		</div>
+		<Grid
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`
+			}}>
+			<GridItem area="nav" bg="coral">
+				Nav
+			</GridItem>
+			<Show above="lg">
+				<GridItem area="aside" bg="gold">
+					aside
+				</GridItem>
+			</Show>
+			<GridItem area="main" bg="dodgerblue">
+				main
+			</GridItem>
+		</Grid>
 	);
 };
 
