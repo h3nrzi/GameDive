74f998cc4594383a54963de29d823c5abae98569
Hossein <rezaeig22@gmail.com>
2023-11-26 18:31:16 +0330

    Fix the problem of bg-color for card in light mode

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
+import { Card, CardBody, HStack, Heading, Image, useColorMode } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
 import getCropImageUrl from '../services/image-url';
 import CriticScore from './CriticScore';
@@ -10,10 +10,13 @@ interface Props {
 }
 
 const GameCard = ({ game }: Props) => {
+	const { colorMode } = useColorMode();
+	const bg = colorMode == 'light' ? 'gray.100' : 'gray.700';
+
 	return (
 		<Card>
 			<Image src={getCropImageUrl(game.background_image)} />
-			<CardBody>
+			<CardBody backgroundColor={bg}>
 				<HStack justifyContent="space-between" marginBottom={3}>
 					<PlatformIconList platforms={game.parent_platforms.map(({ platform }) => platform)} />
 					<CriticScore score={game.metacritic} />
