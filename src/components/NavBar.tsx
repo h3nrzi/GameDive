e0d5f2bb43e50b414754955475a1afe44ea7d4ba
Hossein <rezaeig22@gmail.com>
2023-11-27 11:46:04 +0330

    Fix the Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,18 @@
-import { HStack, Image } from '@chakra-ui/react';
+import { HStack, Image, useColorMode } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo-game-dive.png';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
 
 const NavBar = () => {
+	const { colorMode } = useColorMode();
+	const bg = colorMode === 'light' ? 'gray.800' : '';
+
 	return (
-		<HStack padding="10px">
-			<Image src={logo} boxSize="60px" />
+		<HStack padding="10px" spacing={1}>
+			<Link to="/">
+				<Image src={logo} boxSize="50px" bg={bg} borderRadius="10" objectFit="contain" />
+			</Link>
 			<SearchInput />
 			<ColorModeSwitch />
 		</HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,17 +2,23 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import useGameQueryStore from '../store';
+import { useNavigate } from 'react-router-dom';
 
 const SearchInput = () => {
 	const setSearchTxt = useGameQueryStore((s) => s.setSearchTxt);
 	const ref = useRef<HTMLInputElement>(null);
+	const navigate = useNavigate();
 
 	return (
 		<form
 			onSubmit={(e) => {
 				e.preventDefault();
 
-				if (ref.current) setSearchTxt(ref.current.value);
+				if (ref.current) {
+					setSearchTxt(ref.current.value);
+					navigate('/');
+					ref.current.value = '';
+				}
 			}}>
 			<InputGroup>
 				<InputLeftElement children={<BsSearch />} />
